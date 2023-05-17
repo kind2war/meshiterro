5c6412f8676c7257c6d66787bdaa5084b5ea482b
kind2war <130664778+kind2war@users.noreply.github.com>
2023-05-17 04:15:12 +0000

    [add]ch11

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
 before_action :configure_permitted_parameters, if: :devise_controller?
 
   def after_sign_in_path_for(resource) #サインイン後どこへ遷移するか設定できるメソッド
-   about_path #これでaboutへ上書きした
+   post_images_path
   end
 
   def after_sign_out_path_for(resource)
